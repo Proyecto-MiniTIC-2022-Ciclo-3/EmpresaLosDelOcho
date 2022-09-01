d23f5e30c2418770aa4ea739425937bb886f649a
jhonc <jhon_7246@hotmail.com>
2022-08-31 19:02:35 -0500

    Comentario de prueba

--- src/main/java/com/example/ProyectoEmpresa/ProyectoEmpresaApplication.java
+++ src/main/java/com/example/ProyectoEmpresa/ProyectoEmpresaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProyectoEmpresaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProyectoEmpresaApplication.class, args);
 	}
 
