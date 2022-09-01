52fcd034039af1b061539a86f1a82c781fc6bdfc
barenasr95 <barenas9@cue.edu.co>
2022-08-31 20:44:33 -0500

    Creación Paquetes

---
+++ src/main/java/com/example/ProyectoEmpresa/servicios/ServicioEmpresa.java
@@ -0,0 +1,15 @@
+package com.example.ProyectoEmpresa.servicios;
+
+import com.example.ProyectoEmpresa.entidades.Empresa;
+
+import java.util.List;
+
+public interface ServicioEmpresa {
+    List<Empresa>listarEmpresas();
+    Empresa guardarEmpresas (Empresa empresa);
+    Empresa consultaEmpresasPorNit
+
+
+
+
+}
