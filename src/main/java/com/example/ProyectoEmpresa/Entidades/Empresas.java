24475649a81dedd6f8f0b0622633ad46f4643287
StevCorredor <Stevcorredor@gmail.com>
2022-09-02 17:03:00 -0500

    ajuste de la entidad empresa

---
+++ src/main/java/com/example/ProyectoEmpresa/Entidades/Empresas.java
@@ -0,0 +1,125 @@
+package com.example.ProyectoEmpresa.Entidades;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name="Empresas")
+public class Empresas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "nit_empresa", nullable = false)
+    private long nitEmpresa;
+
+    @Column(name = "nombre_empresa", nullable = false, unique = true)
+    private String nombreEmpresa;
+
+    @Column(name = "documento_empresa", nullable = false, unique = true)
+    private String documentoEmpresa;
+
+    @Column(name = "teléfono_empresa", nullable = false)
+    private String telefonoEmpresa;
+
+    @Column(name = "dirección_empresa", nullable = false)
+    private String direccionEmpresa;
+
+    private List<Empleados>empleados;
+
+    private List<Transacciones>transacciones;
+
+    @Column(name = "fecha_creacion", nullable = false)
+    private Date fechaCreacion;
+
+    @Column(name = "fecha_actualizacion", nullable = false)
+    private Date fechaActualizacion;
+
+    public Empresas(long nitEmpresa, String nombreEmpresa, String documentoEmpresa, String telefonoEmpresa, String direccionEmpresa, List<Empleados> empleados, List<Transacciones> transacciones, Date fechaCreacion, Date fechaActualizacion) {
+        this.nitEmpresa = nitEmpresa;
+        this.nombreEmpresa = nombreEmpresa;
+        this.documentoEmpresa = documentoEmpresa;
+        this.telefonoEmpresa = telefonoEmpresa;
+        this.direccionEmpresa = direccionEmpresa;
+        this.empleados = empleados;
+        this.transacciones = transacciones;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
+    public Empresas() {
+    }
+
+    public long getNitEmpresa() {
+        return nitEmpresa;
+    }
+
+    public void setNitEmpresa(long nitEmpresa) {
+        this.nitEmpresa = nitEmpresa;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getDocumentoEmpresa() {
+        return documentoEmpresa;
+    }
+
+    public void setDocumentoEmpresa(String documentoEmpresa) {
+        this.documentoEmpresa = documentoEmpresa;
+    }
+
+    public String getTelefonoEmpresa() {
+        return telefonoEmpresa;
+    }
+
+    public void setTelefonoEmpresa(String telefonoEmpresa) {
+        this.telefonoEmpresa = telefonoEmpresa;
+    }
+
+    public String getDireccionEmpresa() {
+        return direccionEmpresa;
+    }
+
+    public void setDireccionEmpresa(String direccionEmpresa) {
+        this.direccionEmpresa = direccionEmpresa;
+    }
+
+    public List<Empleados> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleados> empleados) {
+        this.empleados = empleados;
+    }
+
+    public List<Transacciones> getTransacciones() {
+        return transacciones;
+    }
+
+    public void setTransacciones(List<Transacciones> transacciones) {
+        this.transacciones = transacciones;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+
+    public void setFechaActualizacion(Date fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
+}
---
+++ src/main/java/com/example/ProyectoEmpresa/Servicios/ServicioEmpresas.java
@@ -0,0 +1,12 @@
+package com.example.ProyectoEmpresa.Servicios;
+
+import com.example.ProyectoEmpresa.Entidades.Empresas;
+
+import java.util.List;
+
+public interface ServicioEmpresas {
+//    List<Empresas>listarEmpresas();
+//    Empresas guardarEmpresas (Empresas empresa);
+//    Empresas consultaEmpresasPorNit
+
+}
