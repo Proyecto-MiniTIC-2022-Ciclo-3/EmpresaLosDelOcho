e6581ff2f3a70b42f7ed8589d8a11b55ae8fd656
jhonc <jhon_7246@hotmail.com>
2022-09-03 07:37:50 -0500

    Cambio empleados

--- src/main/java/com/example/ProyectoEmpresa/Entidades/Empleados.java
+++ src/main/java/com/example/ProyectoEmpresa/Entidades/Empleados.java
@@ -8,7 +8,6 @@ import java.util.List;
 @Table(name = "Empleados")
 public class Empleados {
 
-   
     @Id  
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_empleados", nullable = false)
