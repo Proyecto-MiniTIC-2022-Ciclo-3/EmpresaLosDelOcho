004127d18c960c2c777b0b314c358829167d287b
StevCorredor <Stevcorredor@gmail.com>
2022-09-02 20:06:05 -0500

    diseño clase empleado

--- src/main/java/com/example/ProyectoEmpresa/Entidades/Empleados.java
+++ src/main/java/com/example/ProyectoEmpresa/Entidades/Empleados.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Table(name = "Empleados")
 public class Empleados {
 
-    @Id
+    @Id  
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_empleados", nullable = false)
     private long idEmpleados;
